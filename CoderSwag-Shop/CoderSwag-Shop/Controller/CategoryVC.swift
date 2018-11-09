203414c471fc6b6a7a90c8b95bb54629a3396473
Dobs Totev <dobstotev@Dobss-MBP.lan>
2018-11-09 10:44:30 +0100

    Build the first screen

--- CoderSwag-Shop/CoderSwag-Shop/Controller/CategoryVC.swift
+++ CoderSwag-Shop/CoderSwag-Shop/Controller/CategoryVC.swift
@@ -8,13 +8,37 @@
 
 import UIKit
 
-class CategoryVC: UIViewController {
+class CategoryVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var categoryTable: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // We need to set Data Source and Delegate of the view
+        categoryTable.dataSource = self
+        categoryTable.delegate = self
+    }
+    
+    // Create the number of rows that a table needs
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // Return the total number of items in the array getCategories
+        return DataService.instance.getCategories().count
+    }
+    
+    // Assigns an item from the array to a specific row
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // Grabbing the table view, and we are going to make a reusable cell so that they replace the currently visible items instead of being hidden below the fold
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell") as? CategoryCell {
+                let category = DataService.instance.getCategories()[indexPath.row]
+                cell.updateViews(category: category)
+                return cell
+        } else {
+            return CategoryCell()
+        }
+        
     }
 
 
--- CoderSwag-Shop/CoderSwag-Shop/View/CategoryCell.swift
+++ CoderSwag-Shop/CoderSwag-Shop/View/CategoryCell.swift
@@ -13,15 +13,10 @@ class CategoryCell: UITableViewCell {
     @IBOutlet weak var categoryImage: UIImageView!
     @IBOutlet weak var categoryTitle: UILabel!
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    // Creating a function to update the views of the table and we are passing in the category to know whichever category is currently in
+    func updateViews(category: Category) {
+        categoryImage.image = UIImage(named: category.imageName)
+        categoryTitle.text = category.title
     }
 
 }
