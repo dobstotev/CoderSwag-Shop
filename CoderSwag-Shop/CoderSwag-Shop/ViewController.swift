333e4686604385a408b56d1972d6a76df1db7f6c
Dobs Totev <dobstotev@Dobss-MBP.lan>
2018-11-09 09:37:27 +0100

    Initial project build

---
+++ CoderSwag-Shop/CoderSwag-Shop/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoderSwag-Shop
+//
+//  Created by Dobs Totev on 09/11/2018.
+//  Copyright © 2018 Dobs Totev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
